6404badfaddb45ef17d3cbc7aeceb2cbf3c6991c
Djiby <djiby.sow.a@gmail.com>
2024-01-09 17:48:23 -0500

    Update ExtractFrameWorkManager.ps1

--- ExtractFrameWorkManager.ps1
+++ ExtractFrameWorkManager.ps1
@@ -2,8 +2,8 @@
 ##***	This script extracts Cognos Framework Manager Models information to CSV files.	***##
 ##***	The script look at an given folder ($ModelsPath), process all model.xml         ***##
 ##***	files located in that folder. The extracted data are available under            ***##
-##***	the folder result.																***##
-##***	Please contact Djiby Sow if you need help.										***##
+##***	the folder named result.							***##
+##***	Please contact @janngomaa if you need help.					***##
 ##*****************************************************************************************##
 
 
